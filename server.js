79738ae0cd051662140aa063d5755ea730183a32
Antti Matikainen <antti.matikainen@sievo.com>
2018-04-13 15:16:42 +0300

    fdasfds

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get("/api/results", (_req, res) => {
   res.setHeader("Content-Type", "application/json");
   res.setHeader("Access-Control-Allow-Origin", "*");
 
-  res.send(buffer);
+  res.json(buffer);
 });
 
 app.post("/api/nodedata", function(req, res) {
